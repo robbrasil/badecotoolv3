c5200726f0fa382d0a5ed92b330e09d9c352885e
robbrasil <robbrasil2089@gmail.com>
2018-02-24 17:15:56 -0500

    Rounded profile image

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -1,6 +1,22 @@
 @extends ('layout')
 
 @section ('content')
+
+	<style>
+.crop{
+	width: 150px;
+  height: 150px;
+  overflow: hidden;
+	border-radius: 50%;
+}
+	.crop img {
+    width: 150px;
+    height: auto;
+    margin: -40px 0 0 0px;
+
+}
+}
+	</style>
 <div class="row">
 	<div class="col-md-4">
 	</div>
@@ -10,7 +26,9 @@
 	<p>Email: {{ Auth::guard('web')->user()->email }}</p>
 
 @if ( Auth::guard('web')->user()->profile_photo )
-<img src="{{Auth::guard('web')->user()->profile_photo}}" width="100px" height="100px" />
+	<div class="crop">
+<img src="{{Auth::guard('web')->user()->profile_photo}}" width="100px" height="auto"  />
+  </div>
 @endif
 
 <!-- <img src="{{asset('avatars/uploadtest.png')}}"/> -->
