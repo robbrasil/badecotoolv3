d983bdbdc72dd500a4e70386eb629a97cb54bfce
robbrasil <robbrasil2089@gmail.com>
2018-02-26 10:54:26 -0500

    u

--- config/database.php
+++ config/database.php
@@ -41,11 +41,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => 'us-cdbr-iron-east-05.cleardb.net',
+            'host' => env('DB_HOST'),
             'port' => env('DB_PORT', '3306'),
-            'database' => 'heroku_3dcefeeac30cdf8',
-            'username' => 'b6f2dc3c1d501c',
-            'password' => '4d6f9827',
+            'database' => env('DB_DATABASE'),
+            'username' => env('DB_USERNAME'),
+            'password' => env('DB_PASSWORD'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
