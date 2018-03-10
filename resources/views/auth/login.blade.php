4a72ee39e926b78453f584e2ab0cdfd7c95ab923
robbrasil <robbrasil2089@gmail.com>
2018-03-09 21:45:46 -0500

    removed forgot password

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -54,9 +54,9 @@
                                 <button type="submit" class="btn btn-primary">
                                     Login
                                 </button>
-                                <a class="btn btn-link" href="{{ route('password.request') }}">
+                                {{-- <a class="btn btn-link" href="{{ route('password.request') }}">
                                     Forgot Your Password?
-                                </a>
+                                </a> --}}
                             </div>
                         </div>
                     </form>
